7841723bb0595dc43c7081068aa679a26cdc3e4f
Pavlo <E:\the_bat>
2021-03-17 19:31:19 +0200

    divide js

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
 
-let bookArr = [
-    {
-        title: "Moby-Dick",
-        genre: "Novel",
-        author: "Herman Melville",
-        read: false,
-        readDate: ""
-    },
-    {
-        title: "White Fang",
-        genre: "Adventure",
-        author: "Jack London",
-        read: true,
-        readDate: ""
-    },
-    {
-        title: "Martin Eden",
-        genre: "Novel",
-        author: "Jack London",
-        read: false,
-        readDate: ""
-    },
-    {
-        title: "Journey to the Center of the Earth ",
-        genre: "Science fiction",
-        author: "Jules Gabriel Verne",
-        read: false,
-        readDate: ""
-    }
-]
-
-
 class BookList {
     constructor() {
         this.wasRead = [];
