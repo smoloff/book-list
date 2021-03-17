95c2e17864ebb5b317b9d63b6bf8b4921d2aef7e
Pavlo <E:\the_bat>
2021-03-17 19:21:25 +0200

    done all except adding boot to next or previous

--- script.js
+++ script.js
@@ -43,20 +43,31 @@ class BookList {
 
     add(book) {
         this.allBooks.push(book);
-        this.allBooks.(book.read) == true ? this.wasRead.push(book) : this.notRead.push(book); // перенос книги в нужный масив
-        console.log("win")
+        // this.allBooks.(book.read) === true ? this.wasRead.push(book) : this.notRead.push(book); // перенос книги в нужный масив
+
 
     };
-    finishCurrentBook() {};
 
-    nextBook;
-    currentBook;
-    lastBook;
-
-
+    finishCurrentBook() {
+        this.allBooks.forEach(element => {
+            if (element.read === true) {
+                this.wasRead.push(element);
+                element.readDate = new Date(Date.now())
+                console.log(element.readDate);
+            } else {
+                this.notRead.push(element);
+            }
+        })
+    }
 }
 
-class Book{
+
+
+
+
+
+
+class Book {
     constructor(title, genre, author, read, readDate) {
         this.title = title;
         this.genre = genre;
@@ -68,6 +79,10 @@ class Book{
 
 let booklist = new BookList();
 
+
+
 bookArr.forEach(element => booklist.add(new Book(element.title, element.genre, element.author, element.read, element.readDate)));
 
-console.log(booklist.allBooks);
+booklist.finishCurrentBook();
+
+console.log(booklist.wasRead);
