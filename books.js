2c36d82b6b481c3cb73be3a8bd4aced3fd07b7c3
evgeniy.holmanskih <evg.holmanskih@gmail.com>
2021-03-18 20:43:00 +0200

    change code structure for finish the book reading

--- books.js
+++ books.js
@@ -3,28 +3,20 @@ let bookArr = [
         title: "Moby-Dick",
         genre: "Novel",
         author: "Herman Melville",
-        read: false,
-        readDate: ""
     },
     {
         title: "White Fang",
         genre: "Adventure",
         author: "Jack London",
-        read: true,
-        readDate: ""
     },
     {
         title: "Martin Eden",
         genre: "Novel",
         author: "Jack London",
-        read: false,
-        readDate: ""
     },
     {
         title: "Journey to the Center of the Earth ",
         genre: "Science fiction",
         author: "Jules Gabriel Verne",
-        read: false,
-        readDate: ""
     }
 ]
--- script.js
+++ script.js
@@ -1,31 +1,55 @@
-
 class BookList {
     constructor() {
-        this.wasRead = [];
-        this.notRead = [];
-        this.read;
-        this.isreading;
-        this.willRead;
+        this.wasReadCount = 0;      // number
+        this.notReadCount = 0;      // number
+
         this.allBooks = []
+
+        this.nextBook = undefined;
+        this.currentBook = undefined;
+        this.lastBook = undefined;
     }
 
     add(book) {
         this.allBooks.push(book);
-        // this.allBooks.(book.read) === true ? this.wasRead.push(book) : this.notRead.push(book); // перенос книги в нужный масив
-
-
+        this.notReadCount++;
     };
 
     finishCurrentBook() {
-        this.allBooks.forEach(element => {
-            if (element.read === true) {
-                this.wasRead.push(element);
-                element.readDate = new Date(Date.now())
-                console.log(element.readDate);
-            } else {
-                this.notRead.push(element);
-            }
-        })
+        // debugger;
+        if(this.currentBook === undefined) {
+            this.currentBook = this.allBooks[0]; // Book object
+            this.nextBook = this.allBooks[1];
+        }
+
+        this.toString();
+
+        // should mark the book that is currently being read as "read"
+        this.currentBook.read = true;
+
+        // Give it a read date of new Date(Date.now())
+        this.currentBook.readDate = Date.now();
+        this.wasReadCount++;
+        this.notReadCount--;
+
+        // Change the last book read to be the book that just got finished
+        this.lastBook = this.currentBook;
+
+        // Change the current book to be the next book to be read
+        this.currentBook = this.nextBook;
+
+        // Change the next book to be read property to be the first unread book you find in the list of books
+        // 1я непрочитанная в списке всех книг
+        this.nextBook = this.allBooks.filter((book) => book.read === false && book !== this.currentBook)[0];
+    }
+
+    toString() {
+        console.log(`
+        next: ${this.nextBook && this.nextBook.toString()};
+        current: ${this.currentBook && this.currentBook.toString()};
+        was read: ${this.wasReadCount};
+        not read: ${this.notReadCount};
+        `)
     }
 }
 
@@ -36,21 +60,24 @@ class BookList {
 
 
 class Book {
-    constructor(title, genre, author, read, readDate) {
+    constructor(title, genre, author) {
         this.title = title;
         this.genre = genre;
         this.author = author;
-        this.read = read;
-        this.readDate = readDate;
+
+        this.read = false;
+        this.readDate = undefined;
+    }
+
+    toString() {
+        return `${this.title} ${this.author}`;
     }
 }
 
 let booklist = new BookList();
-
-
-
-bookArr.forEach(element => booklist.add(new Book(element.title, element.genre, element.author, element.read, element.readDate)));
+bookArr.forEach(element => booklist.add(new Book(element.title, element.genre, element.author)));
 
 booklist.finishCurrentBook();
-
-console.log(booklist.wasRead);
+booklist.finishCurrentBook();
+booklist.finishCurrentBook();
+booklist.finishCurrentBook();
